5822639f4b0b877d37870fe3c0eecf014930e469
felipe_renovato <frenovatoa@outlook.com>
2024-10-13 14:08:34 -0600

    Third practical exercise - done.

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,36 +1,47 @@
 (() => {
 
-    // función para obtener información de una película por Id
-    function getAllMovies( movieId: string ) {
-        console.log({ movieId });
-    }
-
-    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getAllMovieActors( id: string ) {
+    //// Función para obtener información de una película por id.
+    function getMovieById( id: string ) {
         console.log({ id });
     }
 
-    // funcion para obtener el bio del actor por el id
-    function getUsuario( ActorId: string ) {
-        console.log({ ActorId });
+    //// Función para obtener información de los actores de una película.
+    function getMovieCastById( id: string ) {
+        console.log({ id });
+    }
+
+    //// Función para obtener el bio del actor por el id.
+    function getActorBioById( id: string ) {
+        console.log({ id });
     }
     
-    // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[] ) {
+    //// Crear una película.
+    //! Es recoemndable usar máximo 3 parámetros.
+
+    interface Movie {
+        cast:           string[];
+        description:    string;
+        rating:         number;
+        title:          string;
+    }
+
+    function createMovie({ title, description, rating, cast }: Movie) {
         console.log({ title, description, rating, cast });
     }
 
-    // Crea un nuevo actor
-    function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
-        
-        // tarea asincrona para verificar nombre
-        // ..
-        // ..
-        if ( fullName === 'fernando' ) return false;
+    //// Crea un nuevo actor.
 
-        console.log('Crear actor');
-        return true;        
-
+    function checkFullName( fullName: string ) {
+        console.log({ fullName });
+        return true;
     }
-    
+
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+        if ( !checkFullName(fullName) ) return false;
+
+        console.log('Crear actor...');
+        return true;        
+    }
+
 })();
