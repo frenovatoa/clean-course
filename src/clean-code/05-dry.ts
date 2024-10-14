cba42ed2256406b1c54ac63a0766c24ff4b969b3
felipe_renovato <frenovatoa@outlook.com>
2024-10-14 12:50:40 -0600

    Using DRY

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -7,10 +7,26 @@ class Product {
         public size: Size = '',
     ){}
 
+    isProductReady(): boolean {
+        for(const key in this) {
+            switch (typeof this[key]) {
+                case 'string':
+                    if ((<string><unknown>this[key]).length <= 0) throw Error(`${key} is empty!`);
+                    break;
+                case 'number':
+                    if ((<number><unknown>this[key]) <= 0) throw Error(`${key} is zero!`);
+                    break;
+                default:
+                    throw Error(`${typeof this[key]} is not supported!`);
+            }
+        }
+        return true;
+    }
+
     toString() {
-        if (this.name.length <= 0) throw Error('Name is empty!');
-        if (this.price <= 0) throw Error('Price is zero!');
-        if (this.size.length <= 0) throw Error('Size is empty!');
+        
+        if (!this.isProductReady()) return;
+
         return `${this.name} (${this.price}), ${this.size}`;
     }
 }
