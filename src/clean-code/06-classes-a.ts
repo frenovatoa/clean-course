a0aaf13d3c69f04f7c057511abc260a7ac90228d
felipe_renovato <frenovatoa@outlook.com>
2024-10-14 14:06:42 -0600

    Without single responsibility principle.

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,5 +1,7 @@
 (() => {
 
+    //! Sin aplicar el principio de responsabilidad única.
+
     type Gender = 'M' | 'F';
 
     class Person {
@@ -10,7 +12,50 @@
         ) { }
     }
 
-    const newPerson = new Person('Felipe Renovato', 'M', new Date('2000-12-15'));
-    console.log(newPerson);
+    class User extends Person {
+
+        public lastAccess: Date;
+
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(name, gender, birthdate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenedFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(email, role, name, gender, birthdate);
+        }
+    }
+
+    const userSettings = new UserSettings(
+        '/usr/home',
+        '/home',
+        'frenovatoa@outlook.com',
+        'Admin',
+        'Felipe Renovato',
+        'M',
+        new Date('2000-12-15')
+    );
+
+    console.log({ userSettings });
 
 })();
