19c499b407e2925927508b5b52b56a698c3a4e97
felipe_renovato <frenovatoa@outlook.com>
2024-11-03 18:21:20 -0600

    Axios dependency removed (OCP Validation).

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,9 +1,16 @@
-import axios from 'axios';
+//* Si se remueve la dependencia de axios, solo esta clase se verá afectada.
 
 export class HttpClient {
+    // async get(url: string) {
+    //     const { data, status } = await axios.get(url);
+    //     console.log(`GET ${url} - ${status}`);
+    //     return { data, status };
+    // }
     async get(url: string) {
-        const { data, status } = await axios.get(url);
-        console.log(`GET ${url} - ${status}`);
-        return { data, status };
+    
+        const resp = await fetch(url);
+        const data = await resp.json();
+
+        return { data, status: resp.status };
     }
 }
