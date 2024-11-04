6258128942a832ef1aaaddc5583d68819bb221c1
felipe_renovato <frenovatoa@outlook.com>
2024-11-03 18:14:06 -0600

    With OCP

--- src/solid/02-open-close-b.ts
+++ src/solid/02-open-close-b.ts
@@ -1,25 +1,31 @@
-import axios from 'axios';
+import { HttpClient } from './02-open-close-c';
 
 export class TodoService {
 
+    constructor(private httpClient: HttpClient) { }
+
     async getTodoItems() {
-        const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos/');
+        const { data } = await this.httpClient.get('https://jsonplaceholder.typicode.com/todos/');
         return data;
     }
 }
 
 export class PostService {
 
+    constructor(private httpClient: HttpClient) { }
+
     async getPosts() {
-        const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        const { data } = await this.httpClient.get('https://jsonplaceholder.typicode.com/posts');
         return data;
     }
 }
 
 export class PhotosService {
 
+    constructor(private httpClient: HttpClient) { }
+
     async getPhotos() {
-        const { data } = await axios.get('https://jsonplaceholder.typicode.com/photos');
+        const { data } = await this.httpClient.get('https://jsonplaceholder.typicode.com/photos');
         return data;
     }
 
